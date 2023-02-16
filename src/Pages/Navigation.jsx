df926da433924b4545be5b7cf3da594b063be991
Fwed <magione1004@gmail.com>
2023-02-16 12:15:42 +0100

    other test 404

--- src/Pages/Navigation.jsx
+++ src/Pages/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from '../Components/Layout';
+import { Error } from './Error';
 import Home from './Home';
 import { Test } from './Test';
 
@@ -8,12 +9,11 @@ export function Navigation() {
 
     return (
         <Routes>
-           
              <Route element={<Layout />}>
                  <Route index element={<Home />} />
-                <Route path="/test-deploy" element={<Home />} />
-                <Route path="/test-deploy/test" element={<Test />} />
-                
+                <Route exact  path="/test-deploy" element={<Home />} />
+                <Route exact path="/test-deploy/test" element={<Test />} />
+                <Route path="*"  element={<Error />} />
             </Route>
         </Routes>
     );
